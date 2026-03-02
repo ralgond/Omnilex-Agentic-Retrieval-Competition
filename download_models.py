abd4de6a7c9c407e661aeb26a12f212772bbcf81
Teng Huang <ht201509@163.com>
2026-03-02 20:50:35 +0800

    dense retrieve after bm25

--- download_models.py
+++ download_models.py
@@ -1,5 +1,6 @@
 from modelscope import snapshot_download
 
-#model_dir = snapshot_download('Xorbits/Mistral-7B-Instruct-v0.2-GGUF')
-model_dir = snapshot_download('Qwen2.5-32B Q4_K_M', cache_dir='/root/autodl-tmp')
+# model_dir = snapshot_download('Xorbits/Mistral-7B-Instruct-v0.2-GGUF')
+# model_dir = snapshot_download('Qwen2.5-32B Q4_K_M', cache_dir='/root/autodl-tmp')
+model_dir = snapshot_download('BAAI/bge-m3')
 print(f"{model_dir}")
